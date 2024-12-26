cb37d8120bee3ddd700c38ace2fbfbd367af025c
filossoof <filossoof@yandex.ru>
2024-12-26 21:10:52 +0500

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -1,5 +1,6 @@
 my_string = input("учебное заведение: ")
 print("я поступил в ",my_string)
+print(len(my_string))
 print(my_string .upper())
 print(my_string .lower())
 print(my_string .replace(' ' ,''))
